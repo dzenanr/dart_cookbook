69482225696c1424c1c0622b409b681921fbd1b1
dzenanr <dzenanr@gmail.com>
2013-05-31 19:12:43 -0400

    add observable

--- observing_collections/web/observing_collections.dart
+++ observing_collections/web/observing_collections.dart
@@ -1,11 +1,15 @@
 library observe_collections;
 
 // http://observables-articles.dart-lang.appspot.com/web-ui/observables/
+// https://mail.google.com/mail/u/0/?ui=2&zx=yevdhge8k5yw&shva=1#inbox/13efcaff4a796f07
 
 import 'package:web_ui/web_ui.dart';
 
-//final List<DateTime> timestamps = toObservable([]);
-final List<DateTime> timestamps = [];
+@observable
+String text;
+
+final List<DateTime> timestamps = toObservable([]);
+//final List<DateTime> timestamps = [];
 
 void addTimestamp() {
   timestamps.add(new DateTime.now());
