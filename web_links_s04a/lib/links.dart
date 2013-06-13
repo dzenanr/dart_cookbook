d28245347a198d848d1f2af5e96e4265bcef2ab0
dzenanr <dzenanr@gmail.com>
2013-06-13 19:44:04 -0400

    remove dart:uri

--- web_links_s04a/lib/links.dart
+++ web_links_s04a/lib/links.dart
@@ -1,13 +1,11 @@
 library links;
 
-import 'dart:uri';
-
 class Link {
   String name;
   Uri url;
 
   Link(this.name, String link) {
-    url = new Uri(link);
+    url = Uri.parse(link);
   }
 }
 
